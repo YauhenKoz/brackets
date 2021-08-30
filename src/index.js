1b34899db46014fd4c6e83e5c035c5e040121ee4
YauhenKoz <egkozel@gmail.com>
2021-08-30 19:42:38 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  const config = bracketsConfig.map((e) => e.join(''))
+
+  for (let i = 0; i < config.length; i++) {
+
+    if (str.includes(config[i])) {
+      str = str.replace(config[i], '')
+
+      i -= config.length
+    }
+  }
+
+  if (str) return false
+
+  return true
 }
